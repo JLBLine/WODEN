6d6348d52fd7ad25b69d360cc6c1a1a3168220c2
JLBLine <jack.l.b.line@gmail.com>
2020-09-16 16:49:44 +0800

    The maximum value of the shapelet basis functions wasn't large enough for large scale beta values (~> 100 arcmins), so doubled max value, keeping same resolution

--- include/shapelet_basis.h
+++ include/shapelet_basis.h
@@ -1,9 +1,9 @@
 //Number of orders of basis functions
 #define sbf_N 101
 //Length of stored look up arrays
-#define sbf_L 5001
+#define sbf_L 10001
 //Zero pixel of the arrays
-#define sbf_c 2500
+#define sbf_c 5000
 //x resolution of those arrays
 #define sbf_dx 0.01
 
