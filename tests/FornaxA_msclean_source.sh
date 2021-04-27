7c7f85a28b7b38e158415b110a40ac5e7dcb082b
JLBLine <jack.l.b.line@gmail.com>
2021-04-27 11:02:48 +0800

    Begin adding error catching for options in run_woden.py

--- tests/FornaxA_msclean_source.sh
+++ tests/FornaxA_msclean_source.sh
@@ -1,11 +1,15 @@
 mkdir -p data
 
-run_woden.py \
+time run_woden.py \
     --ra0=50.67 --dec0=-37.2 \
-    --num_freq_channels=64 --num_time_steps=5 \
-    --freq_res=20e+3 --time_res=0.5 \
+    --num_freq_channels=16 --num_time_steps=14 \
+    --freq_res=80e+3 --time_res=8.0 \
     --cat_filename=srclist_msclean_fornaxA_phase1+2.txt \
     --metafits_filename=1202815152_metafits_ppds.fits \
-    --band_nums=1,2,3 --output_uvfits_prepend=./data/FornaxA_msclean_zenith \
-    --primary_beam=MWA_FEE \
+    --band_nums=1 --output_uvfits_prepend=./data/FornaxA_msclean_zenith \
+    --primary_beam=None \
+    --hdf5_beam_path=/home/jline/software/useful/mwa_full_embedded_element_pattern.h5 \
     --chunking_size=2000
+    
+    
+#       --hdf5_beam_path=/home/jline/software/useful/mwa_full_embedded_element_pattern.h5 \
