b7f2f4d5a7cde613450dfb98d7ff7e8cfa4564cc
JLBLine <jack.l.b.line@gmail.com>
2021-07-09 15:25:59 +0800

    Started adding example simulations in a dir called 'examples'

---
+++ examples/FornaxA/FornaxA_msclean_imaging.sh
@@ -0,0 +1,16 @@
+mkdir -p images
+#rm images/*
+
+##Convert uvfits to measurement sets. --nologger stops a logging popup
+##that can slow things down. Remove it if you want a graphical logger
+${CASA_DIR}/casa --nologger -c  ${WODEN_DIR}/uv2ms.py \
+  --uvfits_prepend=./data/FornaxA_msclean_band \
+  --band_nums=1,2,3,4,5
+
+wsclean -name ./images/FornaxA_msclean -size 350 350 -niter 80000 \
+  -auto-threshold 0.5 -auto-mask 3 \
+  -pol I -multiscale -weight briggs 0 -scale 0.004 -j 12 -mgain 0.85 \
+  -no-update-model-required \
+  ./data/FornaxA_msclean_band*.ms
+
+rm casa*.log
---
+++ examples/FornaxA/FornaxA_msclean_simulation.sh
@@ -0,0 +1,11 @@
+mkdir -p data
+
+time run_woden.py \
+    --ra0=50.67 --dec0=-37.2 \
+    --num_freq_channels=16 --num_time_steps=14 \
+    --freq_res=80e+3 --time_res=8.0 \
+    --cat_filename=srclist_msclean_fornaxA_phase1+2.txt \
+    --metafits_filename=../metafits/1202815152_metafits_ppds.fits \
+    --band_nums=1,2,3,4,5 \
+    --output_uvfits_prepend=./data/FornaxA_msclean \
+    --primary_beam=MWA_FEE
