c62860c65af225d0ff4953e0ce511f469600f58a
JLBLine <jack.l.b.line@gmail.com>
2024-08-21 08:05:11 +1000

    Another docker script change

--- docker/make_docker_image.sh
+++ docker/make_docker_image.sh
@@ -1,13 +1,13 @@
 ##Build some CUDA versions. Could we just do this with a multi-arch flag?
 for arch in "60" "61" "70" "75" "80" "86"
 do
-    docker build --progress=plain --build-arg="CUDA_ARCH=${arch}" -t jlbline/woden-2.2:cuda-$arch -f Dockerfile_cuda .
-    docker push jlbline/woden-2.2:cuda-$arch
+    docker build --progress=plain --build-arg="CUDA_ARCH=${arch}" -t jlbline/woden-2.3:cuda-$arch -f Dockerfile_cuda .
+    docker push jlbline/woden-2.3:cuda-$arch
 done
 
 ##Build the setonix version. Someone could replace this with a HIP equivalent
 ##of the CUDA stuff above if they can work out how to do it.
-docker build -t jlbline/woden-2.2:setonix -f Dockerfile_setonix .
-
+docker build -t jlbline/woden-2.3:setonix -f Dockerfile_setonix .
+docker push jlbline/woden-2.3:setonix
 
 
